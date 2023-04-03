86c83aeb139d34c2a22b8767042fc94ebef3ee1d
martinRenou <martin.renou@gmail.com>
2023-04-03 17:02:54 +0200

    Remove old mention to logo

--- docs/conf.py
+++ docs/conf.py
@@ -16,10 +16,3 @@ exclude_patterns = []
 html_theme = "pydata_sphinx_theme"
 
 jupyterlite_dir = "."
-
-html_theme_options = {
-   "logo": {
-      "image_light": "xeus-nelson.svg",
-      "image_dark": "xeus-nelson.svg",
-   }
-}
